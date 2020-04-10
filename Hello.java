76f1bb2b0240b3a9ffc8213b084352dd94b6f20f
Dylan <39346569+dc-moses@users.noreply.github.com>
2020-04-10 12:29:05 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
-public class Hello {
-  public static void main{string[] args) {
-  // Prints "Hello, World" in the terminal Window
-  system.out.println("Hello, world");
-  }
-}
+
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
